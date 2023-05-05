200406f462d0924f824f3038b52f4d6256067429
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-05-05 10:25:51 +0200

    api docs preps

--- mnemosyne-web-interface/mnemosyne-api/src/main.ts
+++ mnemosyne-web-interface/mnemosyne-api/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
+import * as fs from 'fs';
+import * as yaml from 'yaml';
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  const port = process.env.API_PORT;
+
+  app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Mnemosyne')
+    .setDescription('Documentation of Mnemosyne API.')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  const yamlString: string = yaml.stringify(document, {});
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+  fs.writeFileSync('./docs/swagger-spec.json', JSON.stringify(document));
+  fs.writeFileSync('./docs/swagger-spec.yaml', yamlString);
+
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port, () => {
+    console.log(
+      `Mnemosyne API has been successfully started on port: ${port}.`
+    );
+  });
+})();
