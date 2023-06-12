30f801cfa5ed816cbab7fa4a3694b0022a73b31f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-06-12 11:36:41 +0200

    swagger docs for proxy api

--- mnemosyne-web-interface/mnemosyne-api-proxy/src/main.ts
+++ mnemosyne-web-interface/mnemosyne-api-proxy/src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as yaml from 'yaml';
 
 (async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PROXY_PORT;
 
   app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Mnemosyne')
+    .setDescription('Documentation of Mnemosyne Proxy API.')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  const yamlString: string = yaml.stringify(document, {});
+
+  fs.writeFileSync('./docs/swagger-spec.json', JSON.stringify(document));
+  fs.writeFileSync('./docs/swagger-spec.yaml', yamlString);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(port, () => {
     console.log(`Front proxy API has been started on port ${port}`);
