230ca45d3a171dbede69712d40257b70f365dcec
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-06-15 09:06:25 +0200

    minor fixes of global message

--- mnemosyne-web-interface/mnemosyne-front/src/app/components/layout-components/global-message/global-message.component.ts
+++ mnemosyne-web-interface/mnemosyne-front/src/app/components/layout-components/global-message/global-message.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalMessageService } from '@shared/global-message.service';
 import { EnvService } from '@shared/env.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'layout-global-message',
   templateUrl: './global-message.component.html',
-  styleUrls: ['./global-message.component.scss']
+  styleUrls: ['./global-message.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0, transform: 'translateY(-10px)' })),
+      transition('void <=> *', animate('300ms ease-in-out'))
+    ])
+  ]
 })
 export class GlobalMessageComponent implements OnInit {
   closeButtonUrl: string;
