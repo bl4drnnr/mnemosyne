3890a5a9b642d5dd564d83e05a7a8bfb766f6c1a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-06-28 11:58:51 +0200

    index page

--- mnemosyne-web-interface/mnemosyne-front/src/app/pages/static/home/home.component.ts
+++ mnemosyne-web-interface/mnemosyne-front/src/app/pages/static/home/home.component.ts
@@ -1,15 +1,53 @@
 import { Component } from '@angular/core';
 import { EnvService } from '@shared/env.service';
+import { Router } from '@angular/router';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('buttonAnimation', [
+      state('hidden', style({ opacity: 0, filter: 'blur(15px)' })),
+      state('visible', style({ opacity: 1, filter: 'blur(0)' })),
+      transition('hidden => visible', animate('1000ms ease-in')),
+      transition('visible => hidden', animate('1000ms ease-out'))
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateY(-80px)',
+          filter: 'blur(10px)'
+        }),
+        animate(
+          '1000ms ease-out',
+          style({ opacity: 1, transform: 'translateY(0)', filter: 'blur(0)' })
+        )
+      ])
+    ])
+  ]
 })
 export class HomeComponent {
-  constructor(private readonly envService: EnvService) {}
+  currentTypingText = 1;
+
+  constructor(
+    private readonly envService: EnvService,
+    private readonly router: Router
+  ) {}
 
   animationOptions = {
     path: `${this.envService.getStaticStorageLink}/animations/blockchain_technology_home.json`
   };
+
+  async handleRedirect(path: string) {
+    await this.router.navigate([path]);
+  }
 }
