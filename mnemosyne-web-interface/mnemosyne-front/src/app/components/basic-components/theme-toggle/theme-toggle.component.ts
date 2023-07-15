6a2fe8685581993a83a564925efbbd7ebeafea8d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2023-07-15 19:56:15 +0200

    theme toggle switch

---
+++ mnemosyne-web-interface/mnemosyne-front/src/app/components/basic-components/theme-toggle/theme-toggle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'basic-theme-toggle',
+  templateUrl: './theme-toggle.component.html',
+  styleUrls: ['./theme-toggle.component.scss']
+})
+export class ThemeToggleComponent {
+  //
+}
