4c94c189da3eaf00adcd17a34ddc8cb6cf2c8135
bl4drnnr <mikhail.bahdashych@protonmail.com>
2024-03-13 14:10:59 +0100

    recovery controller tests

--- mnemosyne-web-interface/mnemosyne-api/src/modules/recovery/recovery.controller.spec.ts
+++ mnemosyne-web-interface/mnemosyne-api/src/modules/recovery/recovery.controller.spec.ts
@@ -1,12 +1,18 @@
+import * as dotenv from 'dotenv';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecoveryController } from './recovery.controller';
 import { AuthGuard } from '@guards/auth.guard';
 import { RecoveryService } from '@modules/recovery.service';
-import {GenerateRecoveryKeysDto} from "@dto/generate-recovery-keys.dto";
-import {Sequelize} from "sequelize-typescript";
-import * as dotenv from 'dotenv';
+import { GenerateRecoveryKeysDto } from '@dto/generate-recovery-keys.dto';
+import { Sequelize } from 'sequelize-typescript';
+import { LoginGenerateRecoveryKeysDto } from '@dto/login-generate-recovery-keys.dto';
+import { RegistrationKeysInterface } from '@interfaces/registration-keys.interface';
+import { LoginKeysInterface } from '@interfaces/login-keys.interface';
+import { GenerateKeysInterface } from '@interfaces/generate-keys.interface';
+import { RecoverAccountInterface } from '@interfaces/recover-account.interface';
+import { RecoverAccountDto } from '@dto/recover-account.dto';
 
-dotenv.config({ path: '.env.test' })
+dotenv.config({ path: '.env.test' });
 
 describe('RecoveryController', () => {
   let controller: RecoveryController;
@@ -14,9 +20,28 @@ describe('RecoveryController', () => {
   let sequelize: Sequelize;
 
   const mockRecoveryService = {
-    registrationGenerateRecoveryKeys: jest.fn().mockImplementation(() => {
-      return ;
-    }),
+    registrationGenerateRecoveryKeys: jest
+      .fn()
+      .mockImplementation(
+        ({ confirmationHash, payload, trx }: RegistrationKeysInterface) => {
+          return;
+        }
+      ),
+    loginGenerateRecoveryKeys: jest
+      .fn()
+      .mockImplementation(({ payload, trx }: LoginKeysInterface) => {
+        return;
+      }),
+    generateRecoveryKeys: jest
+      .fn()
+      .mockImplementation(({ payload, userId, trx }: GenerateKeysInterface) => {
+        return;
+      }),
+    recoverUserAccount: jest
+      .fn()
+      .mockImplementation(({ payload, trx }: RecoverAccountInterface) => {
+        return;
+      })
   };
 
   beforeEach(async () => {
@@ -25,9 +50,9 @@ describe('RecoveryController', () => {
       providers: [
         {
           provide: RecoveryService,
-          useValue: mockRecoveryService,
-        },
-      ],
+          useValue: mockRecoveryService
+        }
+      ]
     })
       .overrideProvider(RecoveryService)
       .useValue(mockRecoveryService)
@@ -51,25 +76,76 @@ describe('RecoveryController', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('registrationGenerateRecoveryKeys', () => {
-    it('should call registrationGenerateRecoveryKeys method with correct parameters', async () => {
+  describe('Registration Generate Recovery Keys', () => {
+    it('Should call registrationGenerateRecoveryKeys method with correct parameters', async () => {
       const confirmationHash = 'testConfirmationHash';
       const payload: GenerateRecoveryKeysDto = { passphrase: '123123123' };
       const trx = await sequelize.transaction();
 
-      await controller.registrationGenerateRecoveryKeys(confirmationHash, payload, trx);
-
-      expect(recoveryService.registrationGenerateRecoveryKeys).toHaveBeenCalledWith({
+      await controller.registrationGenerateRecoveryKeys(
         confirmationHash,
         payload,
-        trx,
+        trx
+      );
+
+      expect(
+        recoveryService.registrationGenerateRecoveryKeys
+      ).toHaveBeenCalledWith({
+        confirmationHash,
+        payload,
+        trx
       });
     });
   });
 
-  describe('loginenerate-recovery-keys', () => {
-    it('', async () => {
+  describe('Login Generate Recovery Keys', () => {
+    it('Should call loginGenerateRecoveryKeys with correct parameters', async () => {
+      const payload: LoginGenerateRecoveryKeysDto = {
+        passphrase: '123123123',
+        email: 'test@test.com',
+        password: '12qw!@QW'
+      };
+      const trx = await sequelize.transaction();
 
-    })
-  })
+      await controller.loginGenerateRecoveryKeys(payload, trx);
+
+      expect(recoveryService.loginGenerateRecoveryKeys).toHaveBeenCalledWith({
+        payload,
+        trx
+      });
+    });
+  });
+
+  describe('Generate Recovery Keys', () => {
+    it('Should call generateRecoveryKeys with correct parameters', async () => {
+      const payload: GenerateRecoveryKeysDto = { passphrase: '123123123' };
+      const userId = 'test-user-id';
+      const trx = await sequelize.transaction();
+
+      await controller.generateRecoveryKeys(payload, userId, trx);
+
+      expect(recoveryService.generateRecoveryKeys).toHaveBeenCalledWith({
+        payload,
+        userId,
+        trx
+      });
+    });
+  });
+
+  describe('Recover Account', () => {
+    it('Should call recoverUserAccount with correct parameters', async () => {
+      const payload: RecoverAccountDto = {
+        passphrase: '123123123',
+        recoveryKeys: ['1', '2', '3', '4', '5']
+      };
+      const trx = await sequelize.transaction();
+
+      await controller.recoverUserAccount(payload, trx);
+
+      expect(recoveryService.recoverUserAccount).toHaveBeenCalledWith({
+        payload,
+        trx
+      });
+    });
+  });
 });
